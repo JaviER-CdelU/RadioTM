69664793cfb7341da02f1b0f35f434c0bedb1afd
JaviER-CdelU <JAVIERGRATTAROLA@GMAIL.COM>
2026-08-10 02:58:00 -0300

    Delete js directory

--- js/firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyBMiRZw3QzxHbhb2PuvSpiXRPW9W58By0Q",
-  authDomain: "tiempo-muerto-radio.firebaseapp.com",
-  databaseURL: "https://tiempo-muerto-radio.firebaseio.com",
-  projectId: "tiempo-muerto-radio",
-  storageBucket: "tiempo-muerto-radio.firebasestorage.app",
-  messagingSenderId: "905096939506",
-  appId: "1:905096939506:web:9196a2d31110082b8acd6b"
-};
