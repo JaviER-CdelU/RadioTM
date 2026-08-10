7f07b0bc09c8bcd83d9e93fb476f1989fd531fc5
JaviER-CdelU <JAVIERGRATTAROLA@GMAIL.COM>
2026-08-10 02:48:58 -0300

    Delete web directory

--- web/js/firebase-config.example.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "PEGAR_API_KEY",
-  authDomain: "TU_PROYECTO.firebaseapp.com",
-  projectId: "TU_PROYECTO",
-  storageBucket: "TU_PROYECTO.appspot.com",
-  messagingSenderId: "PEGAR_MESSAGING_SENDER_ID",
-  appId: "PEGAR_APP_ID"
-};
